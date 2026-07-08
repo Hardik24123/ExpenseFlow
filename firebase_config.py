767a6825121899b21b01963156107f1c460bddc1
hardik24123 <hardikprajapati24123@gmail.com>
2026-07-08 08:18:41 +0530

    Use Firebase credentials from environment

--- firebase_config.py
+++ firebase_config.py
@@ -1,4 +1,6 @@
 import os
+import json
+import base64
 import firebase_admin
 from firebase_admin import credentials, firestore, auth, storage
 from dotenv import load_dotenv
@@ -6,15 +8,22 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def initialize_firebase():
-    cred_path = os.getenv("FIREBASE_CREDENTIALS")
     if not firebase_admin._apps:
-        cred = credentials.Certificate(cred_path)
+        cred_json = base64.b64decode(
+            os.getenv("FIREBASE_CREDENTIALS_BASE64")
+        ).decode("utf-8")
+
+        cred_dict = json.loads(cred_json)
+
+        cred = credentials.Certificate(cred_dict)
+
         firebase_admin.initialize_app(cred, {
-            'storageBucket': 'your-app-id.appspot.com' # Replace with your bucket
+            "storageBucket": "YOUR_BUCKET.appspot.com"
         })
-    
+
     db = firestore.client()
     bucket = storage.bucket()
+
     return db, auth, bucket
 
 db, firebase_auth, firebase_storage = initialize_firebase()
